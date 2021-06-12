6885dc78aa450b25b932fd9585448cba79a20604
noob <noobie37@protonmail.com>
2021-06-12 08:41:18 +0530

    Updates

---
+++ precache-manifest.72b2dbc0a89b89745e95806137616cdf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7160c6b547973e10ae6d60023daaf98",
+    "url": "/get_uptoDate_news/index.html"
+  },
+  {
+    "revision": "d7a035da3d0a8af2417f",
+    "url": "/get_uptoDate_news/static/js/2.2d265f19.chunk.js"
+  },
+  {
+    "revision": "449fda158c7b8742d4a2",
+    "url": "/get_uptoDate_news/static/js/main.3e60c141.chunk.js"
+  },
+  {
+    "revision": "a52ed50ce04b505345ea",
+    "url": "/get_uptoDate_news/static/js/runtime-main.55696a92.js"
+  }
+]);
